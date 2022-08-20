0a047a7f8ddc045ad61cdbd99f3685ba3e8fe1c1
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-20 12:55:37 -0400

    Updated the header with nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
+import Navigation from "./Navigation"
+
 function Header(props) {
     return (
-        <div className="card-panel black lighten-2">
-            <h1 className="red-text text-darken-2">Kimberly Howard</h1>
-        </div>
-        )
+        // <div className="card-panel black lighten-2">
+        //     <h1 className="red-text text-darken-2">Kimberly Howard</h1>
+        //     <Navigation />
+        // </div>
+        <nav>
+            <div className="nav-wrapper">
+                <a href="#!" className="brand-logo center">Kimberly Howard</a>
+                <Navigation />
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+function Navigation() {
+    return (
+        // <div>
+        //     <a href="#">About Me</a>
+        //     <a href="#">Portfolio</a>
+        //     <a href="#">Contact</a>
+        //     <a href="#">Resume</a>
+        // </div>
+        <ul className="left hide-on-med-and-down">
+        <li className="active"><a href="#">About Me</a></li>
+        <li className=""><a href="#">Portfolio</a></li>
+        <li className=""><a href="#">Contact</a></li>
+        <li className=""><a href="#">Resume</a></li>
+    </ul>
+    )
+}
+
+
+
+export default Navigation;
