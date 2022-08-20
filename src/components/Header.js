5771611f578f12beb47f32024cda8a5fe4eba8f8
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-20 12:00:32 -0400

    Added header section

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(props) {
+    return (
+        <div className="card-panel black lighten-2">
+            <h1 className="red-text text-darken-2">Kimberly Howard</h1>
+        </div>
+        )
+}
+
+export default Header;
