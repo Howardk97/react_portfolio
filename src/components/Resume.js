c27546244eced9b365277c5970e38e4d3324f730
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-23 16:23:01 -0400

    updated the resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,15 +3,30 @@ export default function Resume () {
         backgroundImage: "url('stars.jpg')",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        height: "50rem",
+        height: "length|percentage|auto|initial|inherit",
         display: "flex",
         justifyContent: "center",
         alignItems: "center"
     }
 
+    const resume = {
+        color: "white",
+        fontWeight: "bold",
+        margin:"2rem",
+        fontFamily: "'Bebas Neue', cursive",
+        fontSize: "50px"
+    }
+
+    const resumePdf = {
+        marginRight: "2rem",
+        marginLeft: "2rem",
+        marginBottom: "2rem"
+    }
+
     return (
-        <div style={mainPage}>
-            <object data=
+        <div style={mainPage} className="row">
+            <h1 style={resume} className="center">Resume</h1>
+            <object style={resumePdf} className="col-7" data=
             "resume.pdf" 
                 width="800" 
                 height="500"> 
