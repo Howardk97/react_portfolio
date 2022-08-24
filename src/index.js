a89303eeec930d3db9610d153dea26291f3da563
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-24 08:06:17 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
