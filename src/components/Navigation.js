dc3de4b821f94e16f0e33f7379d21b5e45f7654b
KimboSlice <kimberlyhoward529@gmail.com>
2022-09-08 16:52:46 -0400

    Cleaned up code in navbar component and added hover colors on navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,12 +13,10 @@ function Navigation() {
 
     // Stylings
     const navLinks = {
-        // fontWeight: "bold"
         fontFamily: "'Bebas Neue', cursive"
     }
 
     const navbarContainer = {
-        // width: "100%",
         backgroundColor: "#312e81"
     }
 
@@ -32,118 +30,76 @@ function Navigation() {
     let navbarComp;
         if(width < 1000) {
             navbarComp = (
-            // <div>
-            //     <div className="" id="navbarToggleExternalContent">
-            //         <div className="p-4">
-            //             <nav
-            //             className="bg-indigo-900 navbar fixed-top">
-            //             <div className="container-fluid">
-            //                 <button
-            //                 className="navbar-toggler hover:bg-green-300 bg-green-400 focus:bg-green-500"
-            //                 type="button"
-            //                 data-bs-toggle="offcanvas"
-            //                 data-bs-target="#offcanvasNavbar"
-            //                 aria-controls="offcanvasNavbar"
-            //                 >
-            //                 <span className="navbar-toggler-icon"></span>
-            //                 </button>
-            //                 <div className="offcanvas offcanvas-start" id="offcanvasNavbar">
-            //                 <div className="offcanvas-body bg-zinc-200">
-            //                     <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 text-zinc-800">
-            //                     <li className="">
-            //                             <a href="/about">About Me</a>
-            //                         </li>
-            //                         <li className="">
-            //                             <a href="/portfolio">Portfolio</a>
-            //                         </li>
-            //                         <li className="">
-            //                             <a href="/contact">Contact</a>
-            //                         </li>
-            //                         <li className="">
-            //                             <a href="/resume">Resume</a>
-            //                         </li>
-            //                     </ul>
-            //                 </div>
-            //                 </div>
-            //             </div>
-            //             </nav>
-            //         </div>
-            //     </div>
-            // </div>
             <nav className="flex items-center navbar navbar-expand-lg bg-indigo-900 sticky-top">
-                <h1 style={name} className="navbar-brand mx-3" href="#">Kimberly Howard</h1>
-                <button className="bg-indigo-500 mx-3 navbar-toggler hover:bg-indigo-400 focus:bg-indigo-600" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <h1 style={name} 
+                    className="navbar-brand mx-3">
+                    Kimberly Howard
+                </h1>
+                <button className="bg-indigo-500 mx-3 navbar-toggler hover:bg-indigo-400 focus:bg-indigo-600" 
+                    type="button" 
+                    data-bs-toggle="offcanvas" 
+                    data-bs-target="#offcanvasNavbar" 
+                    aria-controls="offcanvasNavbar">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                {/* <div 
-                className="offcanvas offcanvas-end nav bg-indigo-200" 
-                tabindex="-1" 
-                id="offcanvasNavbar" 
-                aria-labelledby="offcanvasNavbar">
-                    <li className='row nav'>
-                        <a href="/about">About Me</a>
-                    </li>
-                    <li>
-                        <a href="/portfolio">Portfolio</a>
-                        <a href="/contact">Contact</a>
-                        <a href="/resume">Resume</a>
-                    </li>
-                </div> */}
-                <div className="offcanvas offcanvas-end" id="offcanvasNavbar">
+                <div className="offcanvas offcanvas-end" 
+                    id="offcanvasNavbar">
                   <div className="offcanvas-body bg-indigo-200">
-                    <ul style={navLinks} className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <ul style={navLinks} 
+                        className="navbar-nav justify-content-end flex-grow-1 pe-3">
                       <li className="nav-item">
-                          <a className="nav-link active text-red-200" href="/about">
+                          <a className="nav-link active text-red-200" 
+                            href="/about">
                               About Me
                           </a>
                       </li>
                       <li className="nav-item">
-                          <a className="nav-link active" href="/portfolio">
+                          <a className="nav-link active" 
+                            href="/portfolio">
                             Portfolio
                           </a>
                       </li>
                       <li className="nav-item">
-                          <a className="nav-link active" href="/contact">
+                          <a className="nav-link active" 
+                            href="/contact">
                             Contact
                           </a>
                       </li>
                       <li className="nav-item">
-                          <a className="nav-link active" href="/resume">
+                          <a className="nav-link active" 
+                            href="/resume">
                             Resume
                           </a>
-                        
                       </li>
                     </ul>
                   </div>
                 </div>
             </nav>
-                                )
-                            } else {
-                                navbarComp = (
-                            <nav style={navbarContainer} className="flex bg-indigo-900 nav">
-                            {/* <a href="/" className="site-title"></a> */}
-                                <h1 style={name} className='self-center m-3'>Kimberly Howard</h1>
-                                <ul className="flex" style={navLinks}>
-                                    <li className="flex items-end">
-                                        <a href="/about">About Me</a>
-                                    </li>
-                                    <li className="">
-                                        <a href="/portfolio">Portfolio</a>
-                                    </li>
-                                    <li className="">
-                                        <a href="/contact">Contact</a>
-                                    </li>
-                                    <li className="">
-                                        <a href="/resume">Resume</a>
-                                    </li>
-                                </ul>
-                        </nav>
-                                )
-                            }
-                        
-
-                        
-                        return <div>{navbarComp}</div>
+            )} else {
+                navbarComp = (
+                    <nav style={navbarContainer} className="flex bg-indigo-900 nav">
+                        <h1 style={name} className='self-center m-3'>Kimberly Howard</h1>
+                        <ul className="flex" style={navLinks}>
+                            <li className="hover:bg-indigo-400">
+                                <a className='hover:bg-indigo-400'
+                                href="/about">About Me</a>
+                            </li>
+                            <li className="">
+                                <a className='hover:bg-indigo-400'
+                                href="/portfolio">Portfolio</a>
+                            </li>
+                            <li className="">
+                                <a className='hover:bg-indigo-400'
+                                href="/contact">Contact</a>
+                            </li>
+                            <li className="">
+                                <a className='hover:bg-indigo-400'
+                                href="/resume">Resume</a>
+                            </li>
+                        </ul>
+                    </nav>
+                )}
+                return <div>{navbarComp}</div>
 }
 
 // Export navigation links
